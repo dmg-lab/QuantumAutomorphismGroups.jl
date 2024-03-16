51d12c6c027b156579c22c314c88895b1b604143
Marcel Wack <wack@math.tu-berlin.de>
2024-03-16 07:11:27 +0100

    changes to cluster job

--- computation/cluster_job.jl
+++ computation/cluster_job.jl
@@ -3,50 +3,36 @@ using QuantumAutomorphismGroups
 
 using CSV
 using DataFrames
-
-csv="../data/data_table.csv"
-
-df = CSV.read(csv, DataFrame) 
-for name in df.Name
-    M = nameToMatroid(String(name))
-    if length(M) >= 5
-        continue
-    end
-    computeLpGbOfMatroid(M,:bases)
-    println("Done with $name")
-end 
-
 #=
-global Droids = []
-for n in 7:7, r in 1:n
+#M = non_fano_matroid()
+#computeLpGbOfMatroid(M,:bases)
+task_number = parse(Int,ENV["SLURM_ARRAY_TASK_ID"])
 
+n_tasks = parse(Int,ENV["SLURM_ARRAY_TASK_COUNT"])
+println("Task number: ", task_number)
+
+
+global Droids = []
+for n in 7:7
     db = Polymake.Polydb.get_db()
     collection = db["Matroids.Small"]
-
-    cursor=Polymake.Polydb.find(collection, Dict("RANK" => r,"N_ELEMENTS"=>n))
-
+    cursor=Polymake.Polydb.find(collection, Dict("N_ELEMENTS"=>n))
     append!(Droids,Matroid.(cursor))
 end
 
-sort!(Droids,by=x->length(getMatroidRelations(x,:bases)[1]))
 
+global myDroids = view(Droids,task_number:n_tasks:length(Droids))
+    
 
-for M in Droids
-    computeGbOfMatroid(M,:bases)
+for M in myDroids
+    computeLpGbOfMatroid(M,:bases)
+    println("Computed for $(getName(M))")
 end
-=#
-#=
-R = "0000*******************************"
-M = matroid_from_revlex_basis_encoding(R,3,7)
-N = matroid_from_nonbases(nonbases(M),6)
-loadInfo(N)
-
-M=uniform_matroid(1,3)
-computeGbOfMatroid(M,:bases)
-
-computeGbOfMatroid(M,:bases)
 
 =#
+
+M = fano_matroid()
+computeLpGbOfMatroid(M,:bases)
 
 
 
